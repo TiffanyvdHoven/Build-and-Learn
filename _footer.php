5456f8ed4c8eb7ecc5b88ef0820e6ad71863b1d1
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2019-09-30 17:18:36 +0200

    Delete _footer.php

--- _footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-  <p class="white-text footer-text"> Tiffany van den Hoven | &copy; <?php echo date('Y') ?> </p>
-  <p> <a class="white-text footer-privacy-link" href="privacy-verklaring.php">Cookies | Privacy</a></p>
-</footer>
