c8226c037f4b23aad3ee1417cf2511b427f3d7bd
TiffanyvdHoven <40486262+TiffanyvdHoven@users.noreply.github.com>
2019-10-06 16:10:39 +0200

    fixed mobile bugs and finished form errors

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ function myFunction() {
 }
 
 document.getElementById("mobile-nav").style.display = 'none';
+
+
+function sendForm(){
+  if(document.getElementById('name').value == ''){
+    // alert("please fill in your name");
+    Swal.fire( 'Error','Please fill in your name','error')
+  }
+  else if(document.getElementById('email').value == ''){
+    Swal.fire( 'Error','Please fill in your e-mail adress','error')
+  }
+  else if (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(document.getElementById('email').value) == false ){
+    Swal.fire( 'Error','Please fill in a correct e-mail adress','error')
+  }
+  else if( document.getElementById('track').value == ''){
+    Swal.fire( 'Error','Please choose a track','error')
+  }
+  else{
+    Swal.fire('Thank you!','Your registration has been submitted sucessfully','success').then(function(){
+      location.reload();
+    });
+  }
+}
